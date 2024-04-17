74f6eca4043a2ddd1a0d0861978a2c73fa250cba
Wea3el <wesleyz2004@gmail.com>
2024-04-17 17:33:20 -0400

    virtual env and such

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+@app.route("/bye")
+
+def bye_world():
+    return render_template("appeals.html")
+
+
+@app.route("/name/<name>")
+
+def hello_name(name):
+    return "Hello " + name
+if __name__ == '__main__':
+    app.run(debug = True)
