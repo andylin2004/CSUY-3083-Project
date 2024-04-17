f1cffafc0343901703c4e07c0c9b60c20f7b4ef4
Wea3el <wesleyz2004@gmail.com>
2024-04-17 17:44:38 -0400

    added server.py file and configured it

--- server.py
+++ server.py
@@ -4,18 +4,51 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def login():
+    return render_template("index.html")
+
+
+@app.route("/home")
 def home():
-    return "Hello World"
+    return render_template("home.html")
 
-@app.route("/bye")
-
-def bye_world():
+@app.route("/appeals")
+def appeals():
     return render_template("appeals.html")
 
 
-@app.route("/name/<name>")
+@app.route("/charges")
+def charges():
+    return render_template("charges.html")
 
-def hello_name(name):
-    return "Hello " + name
+
+@app.route("/crimes")
+def crimes():
+    return render_template("crimes.html")
+
+@app.route("/criminals")
+def criminals():
+    return render_template("criminals.html")
+
+@app.route("/officers")
+def officers():
+    return render_template("officers.html")
+
+
+@app.route("/probation_officers")
+def probation_officers():
+    return render_template("probation_officers.html")
+
+@app.route("/sentences")
+def sentences():
+    return render_template("sentences.html")
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
