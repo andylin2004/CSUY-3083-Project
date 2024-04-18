dd6df412339cfa1cbb2efc5c07a50d68f59b8f1d
Andy Lin <andy.lin.2004@outlook.com>
2024-04-17 23:02:16 -0400

    basic working account sign up functionality

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,flash
 import pymysql
 import pandas as pd
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "criminals"
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 connection = pymysql.connect(host="localhost",
                              user="root",
@@ -24,10 +25,6 @@ def runstatement(statement):
         df = pd.DataFrame(results, columns=column_names)
     cursor.close()
     return df
-
-
-
-
 
 @app.route("/")
 def login():
@@ -53,7 +50,6 @@ def charges():
         datas.append(j.to_dict())
     return render_template("charges.html", charges=datas)
 
-
 @app.route("/crimes")
 def crimes():
     dn = runstatement("CALL get_crimes();")
@@ -78,7 +74,6 @@ def officers():
         datas.append(j.to_dict())
     return render_template("officers.html", officers=datas)
 
-
 @app.route("/probation_officers")
 def probation_officers():
     dn = runstatement("CALL get_prob_officers();")
@@ -86,7 +81,6 @@ def probation_officers():
     for i,j in dn.iterrows():
         datas.append(j.to_dict())
     return render_template("probation_officers.html", prob_officers=datas)
-
 
 @app.route("/sentences")
 def sentences():
@@ -96,12 +90,28 @@ def sentences():
         datas.append(j.to_dict())
     return render_template("sentences.html", sentences=datas)
 
-
-
-
-
-
-
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    clearance_id = request.form.get("clearance_id")
+    if len(password) < 8:
+        flash('Your password is too short!', 'error')
+        return redirect("/")
+    elif len(password) > 16:
+        flash('Your password is too long!', 'error')
+        return redirect("/")
+    else:
+        if len(username) == 0:
+            flash('You didn\'t enter a username!', 'error')
+            return redirect("/")
+        elif len(clearance_id) == 0:
+            flash('You didn\'t enter a clearance ID!', 'error')
+            return redirect("/")
+        else:
+            password = hash(password)
+            return redirect("/home")
+        
 
 if __name__ == '__main__':
     app.run(debug = True)
