41314badbfc5b4be9299d27b84818843fb551418
Andy Lin <andy.lin.2004@outlook.com>
2024-04-21 10:32:24 -0700

    moved some shared components to a script static file

---
+++ static/script.js
@@ -0,0 +1,37 @@
+function editFieldsIn(id_name) {
+    var baseID = "#" + id_name;
+    document.querySelectorAll(baseID + " input, " + baseID + " select").forEach(function(element) {
+        element.removeAttribute("disabled");
+    });
+}
+
+function endEditFieldsIn(id_name) {
+    var baseID = "#" + id_name;
+    document.querySelectorAll(baseID + " input, " + baseID + " select").forEach(function(element) {
+        element.setAttribute("disabled", true);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", event => {
+    var queryStr = window.location.search;
+    var urlParams = new URLSearchParams(queryStr);
+    
+    var category = urlParams.get('column');
+    var query = urlParams.get('query');
+    var embed = urlParams.get('embed');
+    
+    if (category != null) {
+        document.getElementById('column').value = category;
+        document.getElementById('query').value = query;
+    }
+    
+    if (embed) {
+        document.querySelectorAll(".hideable *, .hideable").forEach(function(element) {
+            element.style.display = 'none';
+        })
+        document.querySelectorAll(".canBeNewTab").forEach(function(element) {
+            element.removeAttribute("data-bs-toggle");
+            element.removeAttribute("data-bs-target");
+        })
+    }
+});
